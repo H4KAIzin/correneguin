27a23bbb7a0a5967d60a9aabe460593eddb493a6
luquitas <viniciaosantosdacosta37378@gmail.com>
2024-11-04 08:52:50 -0300

    W,AA,2,Q,R,E,AA,AA

--- correneguin/MainPage.xaml.cs
+++ correneguin/MainPage.xaml.cs
@@ -30,9 +30,9 @@ int alturaJanela=0;
 		velocidade1=(int)(width*0.001);
 		velocidade2=(int)(width*0.004);
 		velocidade3=(int)(width*0.008);
-		velocidade4=(int)(width*0.0010);
-		velocidade5=(int)(width*001);
-		velocidade=(int)(width*001);
+		velocidade4=(int)(width*0.012);
+		velocidade5=(int)(width*0.016);
+		velocidade=(int)(width*0.01);
 	}
 
 	void CorrigeTamanhoCenario(double width, double height)
@@ -40,8 +40,12 @@ int alturaJanela=0;
 		foreach (var a in HSLayer1.Children)
 		(a as Image). WidthRequest = width;
 		foreach (var a in HSLayer2.Children)
-		(a as Image). WidthRequestRequest = width;
+		(a as Image). WidthRequest = width;
 		foreach (var a in HSLayer3.Children)
+		(a as Image). WidthRequest = width;
+		foreach(var a in HSLayer4.Children)
+		(a as Image). WidthRequest = width;
+		foreach (var a in HSLayer5.Children)
 		(a as Image). WidthRequest = width;
 		foreach(var a in HSLayerChao.Children)
 		(a as Image). WidthRequest = width;
@@ -49,10 +53,57 @@ int alturaJanela=0;
 		HSLayer1.WidthRequest = width*1.5;
 		HSLayer2.WidthRequest = width*1.5;
 		HSLayer3.WidthRequest = width*1.5;
+		HSLayer4.WidthRequest = width*1.5;
+		HSLayer5.WidthRequest = width*1.5;
 		HSLayerChao.WidthRequest = width*1.5;
 	}
 
+	void GerenciaCenario()
+	{
+		MoveCenario();
+		GerenciaCenario(HSLayer1);
+		GerenciaCenario(HSLayer2);
+		GerenciaCenario(HSLayer3);
+		GerenciaCenario(HSLayer4);
+		GerenciaCenario(HSLayer5);
+		GerenciaCenario(HSLayerChao);
+	}
 	
+	void MoveCenario()
+	{
+		HSLayer1.TranslationX -= velocidade1;
+		HSLayer2.TranslationX -= velocidade2;
+		HSLayer3.TranslationX -= velocidade3;
+		HSLayer4.TranslationX -= velocidade4;
+		HSLayer5.TranslationX -= velocidade5;
+		HSLayerChao.TranslationX -= velocidade;
+	}
+
+	void GerenciaCenario(HorizontalStackLayout HSL)
+	{
+		var view = (HSL.Children.First() as Image);
+		if (view.WidthRequest + HSL.TranslationX < 0)
+		{
+			HSL.Children.Remove(view);
+			HSL.Children.Add(view);
+			HSL.TranslationX = view.TranslationX;
+		}	
+	}
+
+	async Task Desenha()
+	{
+		while(!estaMorto)
+		{
+			GerenciaCenario();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenha();
+    }
 
 }
 
